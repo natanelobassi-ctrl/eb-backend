7292197afb7ab8447cf316b5016e21a2a73dfc55
natanelobassi-ctrl <natanelobassi@gmail.com>
2026-08-30 06:03:54 +0100

    Add route for '/eleve' using eleveRoutes

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.use('/auth', authRoutes);
 app.use('/classe', classeRoutes);
+app.use('/eleve', eleveRoutes);
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ error: 'Erreur serveur.' });
